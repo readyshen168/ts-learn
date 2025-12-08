a61f1070dbe5d513af8e6fb7b93d9d6dfb2386f7
Ubuntu <ubuntu@localhost.localdomain>
2025-12-09 01:19:33 +0800

    demo2_9版本2, 加入了对padding参数的修正(确保其为非负整数);在tsconfig.json中加入ES2022 lib, 以确保在demo2_9中可以使用repeat方法

--- dist/demo2_9.js
+++ dist/demo2_9.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = padLeft;
+// 在字符串左边加指定数量空格的方法
 function padLeft(value, padding) {
     if (typeof padding === "number") {
-        return Array(padding + 1).join(" ") + value;
+        var n = Math.max(0, Math.floor(padding));
+        return " ".repeat(n) + value;
     }
     if (typeof padding === "string") {
         return padding + value;
--- src/demo2_9.ts
+++ src/demo2_9.ts
@@ -1,7 +1,8 @@
 // 在字符串左边加指定数量空格的方法
 export default function padLeft(value: string, padding: string | number): string {
     if(typeof padding === "number"){
-        return Array(padding + 1).join(" ") + value;
+        const n = Math.max(0, Math.floor(padding));
+        return " ".repeat(n) + value;
     }
 
     if(typeof padding === "string"){
